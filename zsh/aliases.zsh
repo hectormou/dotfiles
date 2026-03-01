e08db3bd661ef04a46ee13a46cfe2955018db724
hectormou <hcmoure@gmail.com>
2026-03-01 22:09:06 +0100

    fin de pruebas con el cherry-pick

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,14 +1,16 @@
-alias brave='setsid brave > /dev/null'
+#SEARCH alias
+alias brave='brave &> /dev/null'
 alias github='brave "github.com"'
 alias campus='brave "cv.usc.es/my/courses.php"'
 alias gemini='brave "https://gemini.google.com/app"'
 alias apuntes='brave "github.com/adrianql5/2-TEORIA-2-CUATRI/tree/main"'
 alias matrix='brave "https://matrixcalc.org.es/es/"'
-alias obsidian='setsid obsidian > /dev/null'
 alias nube='brave "https://goo.su/a7IGC"'
 alias chess='brave "https://www.chess.com/home"'
 alias spotify='brave "https://open.spotify.com/intl-es/"'
 alias outlook='brave "https://outlook.office.com/mail/0/?deeplink=mail%2F0%2F%3Fnlp%3D0"'
 alias cookie='brave "https://orteil.dashnet.org/cookieclicker/"'
+
+#NVIM alias
 alias zshconfig='nvim $ZDOTDIR/.zshrc'
 alias zshalias='nvim $ZDOTDIR/aliases.zsh'
